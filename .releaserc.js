fd8dfa1964e700591a7535e8f8f709289cf59402
laxminarayankatker <laxminarayan.katker@gmail.com>
2022-03-11 15:37:00 +0530

    Create .releaserc.js

---
+++ .releaserc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/github",
+        ["@semantic-release/release-notes-generator", {
+            "parserOpts": {
+                "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"]
+            },
+            "writerOpts": {
+                "commitsSort": ["subject", "scope"]
+            }
+        }],
+    ],
+    branches: [
+        'main'
+    ],
+}
